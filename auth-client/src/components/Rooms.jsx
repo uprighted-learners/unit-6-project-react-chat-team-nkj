6423bc1c534ce0aed830b5d8d5ca01d95a79f2fb
jamere mccall <jameremccall@gmail.com>
2025-04-07 20:53:53 -0400

    auth

---
+++ auth-client/src/components/Rooms.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Rooms = ({setRoomId}) => {
+  const [rooms, setRooms] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch("http://localhost:8080/api/rooms", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        console.log(data)
+        setRooms(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchRooms();
+  }, []);
+
+  const handleRoomClick = (roomId) => { 
+    setRoomId(roomId);
+    navigate(`/rooms/${roomId}`);
+  };
+
+  return (
+    <div>
+      <h2>Available Rooms</h2>
+      <ul>
+        {rooms.map((room) => (
+          <li key={room._id} onClick={() => handleRoomClick(room._id)}>
+            {room.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Rooms;
